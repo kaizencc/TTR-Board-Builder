e2958cab1ec093c0cb59105f18b3ca3de6b4fdfc
Dominic Chui <dominic.chui@outlook.com>
2020-05-05 22:13:02 -0400

    initial

---
+++ Ticket To Ride Board Builder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ticket To Ride Board Builder
+//
+//  Created by Dominic Chui on 5/5/20.
+//  Copyright © 2020 CS326. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Ticket To Ride Board BuilderTests/Ticket_To_Ride_Board_BuilderTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ticket_To_Ride_Board_BuilderTests.swift
+//  Ticket To Ride Board BuilderTests
+//
+//  Created by Dominic Chui on 5/5/20.
+//  Copyright © 2020 CS326. All rights reserved.
+//
+
+import XCTest
+@testable import Ticket_To_Ride_Board_Builder
+
+class Ticket_To_Ride_Board_BuilderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
